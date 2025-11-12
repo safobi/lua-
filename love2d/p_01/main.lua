db95a9a62abbb5dab4c5b394e5239bf4e46241fc
safobi <fasbine@gmail.com>
2025-11-12 04:14:12 +0200

    1

--- love2d/p_01/main.lua
+++ love2d/p_01/main.lua
@@ -1,4 +1,36 @@
-function love.draw()
-	love.graphics.polygon("line",100,100, 200,100, 150,200)
 
+function love.load()
+	canvas = love.graphics.newCanvas(600, 600)
+ 
+	-- Rectangle is drawn to the canvas with the regular alpha blend mode.
+	love.graphics.setCanvas(canvas)
+		love.graphics.clear()
+		love.graphics.setBlendMode("alpha")
+		love.graphics.setColor(0, 0.5, 0, 0.5)
+--		love.graphics.rectangle('fill', 0, 0, 100, 100)
+		love.graphics.circle('fill', 300, 300, 300)
+	love.graphics.setCanvas()
+	
+	
+end
+
+
+ 
+function love.draw()
+	-- very important!: reset color before drawing to canvas to have colors properly displayed
+	-- see discussion here: https://love2d.org/forums/viewtopic.php?f=4&p=211418#p211418
+	love.graphics.setColor(1, 1, 1, 1)
+	love.graphics.points(300, 300)
+	-- The rectangle from the Canvas was already alpha blended.
+	-- Use the premultiplied alpha blend mode when drawing the Canvas itself to prevent improper blending.
+--	love.graphics.setBlendMode("alpha", "premultiplied")
+--	love.graphics.draw(canvas)
+	-- Observe the difference if the Canvas is drawn with the regular alpha blend mode instead.
+	love.graphics.setBlendMode("alpha")
+	love.graphics.draw(canvas, 200, 0)
+ 
+	-- Rectangle is drawn directly to the screen with the regular alpha blend mode.
+--	love.graphics.setBlendMode("alpha")
+--	love.graphics.setColor(1, 0, 0, 0.5)
+--	love.graphics.rectangle('fill', 200, 0, 100, 100)
 end
