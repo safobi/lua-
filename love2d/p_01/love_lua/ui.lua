54cb233a12343ebfe4fc0c5fcfdc179c70ca0b7c
safobi <fasbine@gmail.com>
2025-12-02 17:16:22 +0200

    123

--- love2d/p_01/love_lua/ui.lua
+++ love2d/p_01/love_lua/ui.lua
@@ -129,11 +129,30 @@ function tabs_mains:update_mains()
 end
 
 function draw_in_borders(tab,area)
+	local res={}
+	local x_ox,y_oy=area.ox,area.oy
 	local gap_x,gap_y=tab.gap_x,tab.gap_y
-	--local x,y=area.coord.x,area.coord.y
-	local t_h,t_w=area.source:getHeight()*area.coord.sy,area.source:getWidth()*area.coord.sx
-	
-
+	local x_or,y_or=tab.origin_x,tab.origin_y
+	local x_th,y_th=tab.x_th,tab.y_th
+	local x_c,y_c=0,0
+	local y_lol=false
+	function dfs(x,y)
+		if ((x+gap_x+(2*x_ox))>x_th) or ((y+gap_y+(2*y_oy))>y_th) then return end
+		--if ((y+gap_y+(2*y_oy))>y_th)  then return end
+		if y_lol==false then
+			dfs(x,gap_y+y_oy*2+y)
+		end
+		y_lol=true
+		y_c=gap_y+y_oy+y
+		x_c=gap_x+x_ox+x
+		res[#res+1]={x_c,y_c}
+		-- print( "x " .. x_c.." y " .. y_c)
+		 -- print( "x " .. x.." y " .. y)
+		dfs(gap_x+x_ox*2+x,y)
+		y_lol=false
+	end
+	dfs(x_or,y_or,0,0)
+	return res
 end
 
 
